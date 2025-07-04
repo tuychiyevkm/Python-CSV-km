12351b048667c952f45612cbafe66e0f8f04ee64
tuychiyevkm <tmkomron@gmail.com>
2025-07-04 07:29:17 +0500

    yangilandi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,20 @@
+import csv
+
+with open('students.csv') as f:
+    reader = csv.DictReader(f)
+    students = list(reader)
+
+students_sorted = sorted(students, key=lambda x: int(x['score']), reverse=True)
+
+with open('rating.csv', 'w') as f2:
+    fieldnames = ['rank', 'name', 'score']
+    writer = csv.DictWriter(f2, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for i, student in enumerate(students_sorted, start=1):
+        writer.writerow({
+            'rank': i,
+            'name': student['name'],
+            'score': student['score']
+        })
 
